867330833c432cdf75dfd7224e4038f359e70c87
Mark Alston <malston@pivotal.io>
2015-11-29 18:22:38 -0700

    Update homepage

--- sparrow-web/src/main/java/com/comcast/sparrow/web/SparrowWebApplication.java
+++ sparrow-web/src/main/java/com/comcast/sparrow/web/SparrowWebApplication.java
@@ -21,6 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
@@ -63,8 +64,10 @@ public class SparrowWebApplication {
     	@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http.httpBasic().and().authorizeRequests()
-					.antMatchers("/index.html", "/views/home.html", "/views/login.html", "/").permitAll().anyRequest()
-					.authenticated().and().csrf()
+//					.antMatchers("/index.html", "/home.html", "/", "/login", "/beans").permitAll()
+					.antMatchers("/index.html", "/views/home.html", "/views/login.html", "/", "/beans").permitAll()
+					.antMatchers(HttpMethod.GET, "/recommendations/**","/reviews/**","/people/**","/movie/**","/catalog/**","/movies/**","/genres/**","/likes/**").permitAll()
+					.anyRequest().authenticated().and().csrf()
 					.csrfTokenRepository(csrfTokenRepository()).and()
 					.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
 		}
